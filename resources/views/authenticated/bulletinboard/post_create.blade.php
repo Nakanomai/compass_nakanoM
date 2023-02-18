b335fb6d54109064f5da8d63e1058999d0a43a19
Nakano mai <>
2023-02-19 00:40:33 +0900

    サブカテゴリー追加

--- resources/views/authenticated/bulletinboard/post_create.blade.php
+++ resources/views/authenticated/bulletinboard/post_create.blade.php
@@ -44,6 +44,13 @@
         <input type="submit" value="追加" class="w-100 btn btn-primary p-0" form="mainCategoryRequest">
       </div>
       <!-- サブカテゴリー追加 -->
+      @if(auth()->user()->role != 4)
+      <div class="">
+        <p class="m-0">サブカテゴリー</p>
+        <input type="text" class="w-100" name="main_category_name" form="mainCategoryRequest">
+        <input type="submit" value="追加" class="w-100 btn btn-primary p-0" form="mainCategoryRequest">
+      </div>
+      @endif
       <form action="{{ route('main.category.create') }}" method="post" id="mainCategoryRequest">{{ csrf_field() }}</form>
     </div>
   </div>
