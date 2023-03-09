b4043e3f8f871ed54a2b3674916f5af2944d58e4
Nakano mai <>
2023-03-09 19:25:05 +0900

    選択科目の表示

--- resources/views/authenticated/users/search.blade.php
+++ resources/views/authenticated/users/search.blade.php
@@ -44,6 +44,9 @@
       <div>
         @if($user->role == 4)
         <span>選択科目 :</span>
+        @foreach($user->subjects as $subject)
+        <span>{{ $subject->subject }}</span>
+        @endforeach
         @endif
       </div>
     </div>
@@ -88,6 +91,12 @@
           </div>
           <div class="selected_engineer">
             <label>選択科目</label>
+            @foreach($subjects as $subject)
+            <div class="">
+              <input type="checkbox" name="subject[]" value="{{ $subject->id }}">
+              <label>{{ $subject->subject }}</label>
+            </div>
+            @endforeach
           </div>
         </div>
       </div>
