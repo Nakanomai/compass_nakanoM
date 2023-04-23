95c1aae6b0a86bc1ccf4991986a82380cad89c97
Nakano mai <>
2023-04-23 23:42:58 +0900

    form属性追加

--- resources/views/authenticated/calendar/general/calendar.blade.php
+++ resources/views/authenticated/calendar/general/calendar.blade.php
@@ -16,18 +16,18 @@
         <form action="{{ ('deleteParts') }}" method="post">
           <div class="w-100">
             <div class="modal-inner-date w-50 m-auto">
-              <p>予定日：</p><input type="text" style="border: none;" name="date_title" class="w-100">
+              <p>予定日：</p><input type="text" style="border: none;" name="date_title" class="w-100" form="deleteParts">
             </div>
             <div class="modal-inner-time w-50 m-auto pt-3 pb-3">
-              <p>時間：</p><input type="text" style="border: none;" name="time_title" class="w-100">
+              <p>時間：</p><input type="text" style="border: none;" name="time_title" class="w-100"　form="deleteParts">
             </div>
             <div class="w-100 cancel_message pt-3 pb-3">
               上記の予約をキャンセルしてもよろしいでしょうか？
             </div>
             <div class="w-50 m-auto edit-modal-btn d-flex">
               <a class="js-modal-close btn btn-primary d-inline-block" href="">閉じる</a>
-              <input type="hidden" class="edit-modal-hidden" name="post_id" value="">
-              <input type="submit" class="btn btn-danger d-block" value="キャンセル">
+              <input type="hidden" class="edit-modal-hidden" name="post_id" value="" form="deleteParts">
+              <input type="submit" class="btn btn-danger d-block" value="キャンセル" form="deleteParts">
             </div>
           </div>
           {{ csrf_field() }}
